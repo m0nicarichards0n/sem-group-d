cd2c0d9c1f23cbb0a59eb689fd11d550956bac59
Grzegorz Samociak <40418257@live.napier.ac.uk>
2019-02-04 23:54:42 +0000

    first select

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -11,6 +11,10 @@ public class App
 
         // Connect to database
         a.connect();
+        // Get Employee
+        Country emp = a.getCountry(255530);
+        // Display results
+        a.displayCountry(emp);
 
         // Disconnect from database
         a.disconnect();
@@ -78,6 +82,74 @@ public class App
             {
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+
+    public Country getCountry(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT * "
+                    + "FROM country "
+                    + "WHERE Name = 'Poland'";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new country if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Country country = new Country();
+                country.Name = rset.getString("Name");
+                country.Continent = rset.getString("Continent");
+                country.Region = rset.getString("Region");
+                country.SurfaceArea = rset.getInt("SurfaceArea");
+                country.IndepYear = rset.getInt("IndepYear");
+                country.Population = rset.getInt("Population");
+                country.LifeExpectancy = rset.getInt("LifeExpectancy");
+                country.GNP = rset.getInt("GNP");
+                country.GNPOld = rset.getInt("GNPOld");
+                country.LocalName = rset.getString("LocalName");
+                country.GovernmentForm = rset.getString("GovernmentForm");
+                country.HeadOfState = rset.getString("HeadOfState");
+                country.Capital = rset.getString("Capital");
+                country.Code2 = rset.getString("Code2");
+                return country;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
+    public void displayCountry(Country country)
+    {
+        if (country != null)
+        {
+            System.out.println(
+                    country.Code + " "
+                    + country.Name + " "
+                    + country.Continent + " "
+                    + country.Region + " "
+                    + country.SurfaceArea + " "
+                    + country.IndepYear + " "
+                    + country.Population + " "
+                    + country.LifeExpectancy + " "
+                    + country.GNP + " "
+                    + country.GNPOld + " "
+                    + country.LocalName + " "
+                    + country.GovernmentForm + " "
+                    + country.HeadOfState + " "
+                    + country.Capital + " "
+                    + country.Code2 + "\n");
         }
     }
 }
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,26 @@
+package com.napier.sem;
+
+public class Country
+{
+    public String Code;
+    public String Name;
+    public String Continent;
+    public String Region;
+    public double SurfaceArea;
+    public int IndepYear;
+    public int Population;
+    public double LifeExpectancy;
+    public double GNP;
+    public double GNPOld;
+    public String LocalName;
+    public String GovernmentForm;
+    public String HeadOfState;
+    public String Capital;
+    public String Code2;
+
+
+
+    public Country(){
+
+    }
+}
