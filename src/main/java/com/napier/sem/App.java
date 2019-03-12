59741e33890c2b983d00d23422765448567fa1fd
Grzegorz Samociak <40418257@live.napier.ac.uk>
2019-03-12 07:46:56 +0000

    Adding Mock Deploy Stage

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,7 +9,14 @@ public class App {
         App a = new App();
 
         // Connect to database
-        a.connect("localhost:33060");
+        if (args.length < 1)
+        {
+            a.connect("localhost:3306");
+        }
+        else
+        {
+            a.connect(args[0]);
+        }
 
         // Display user options and display appropriate tables based on request
         a.displayUserSelection();
