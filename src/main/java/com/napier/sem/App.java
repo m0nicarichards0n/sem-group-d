9daa15d304f6436daa0cc79f679153492e6ab91f
Grzegorz Samociak <40418257@live.napier.ac.uk>
2019-03-12 07:13:35 +0000

    string assert test

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -42,7 +42,7 @@ public class App {
                 // Wait a bit for db to start
                 Thread.sleep(2000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://"+ location +"/world?allowPublicKeyRetrieval=true?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://"+ location +"/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("db");
+    }
+
+    @Test
+    void testGetCountries()
+    {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        //final PrintStream originalOut = System.out;
+
+        DisplayResults displayResults = new DisplayResults();
+        System.setOut(new PrintStream(outContent));
+        displayResults.countriesInWorld();
+        assertEquals("-----------------------------------------------------------------------------------------------------------------",
+                outContent.toString());
+    }
+}
